5d6232c70590dc256e357f1bc8fbf194ba2512a0
naomi <nbeck1@mail.yu.edu>
2025-06-17 15:50:21 -0400

    download script

--- 01_download_notes_data.sh
+++ 01_download_notes_data.sh
@@ -6,3 +6,4 @@ curl -o birdwatch.gz https://ton.twimg.com/birdwatch-public-data/2025/06/16/note
 # update the timestamp on the resulting file using touch  
 # do not remove, this will keep make happy and avoid re-downloading of the data once you have it
 touch birdwatch.gz
+c:\Users\ds3\Documents\My_ds3\coursework\week3\ngrams\Makefile
